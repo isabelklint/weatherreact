0224fa9ba13b099885662cfbb3fa4b68b69459e2
Isabel Klint <isabel.klint@gmail.com>
2022-08-21 21:10:33 -0500

    updated git url

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
         <header className="App-header"><h1>LOFI WEATHER APP</h1></header >
         <SearchWeather />
+        <p><a href="https://github.com/isabelklint/weatherreact">Open-source code</a> by <a href="https://www.linkedin.com/in/isabel-klint/">Isabel Klint</a>, <a href="https://www.shecodes.io/">SheCodes</a> class project.</p>
     </div>
   );
 }
